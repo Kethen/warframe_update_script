4a9c7a6815d6bf2ba51db4d814fa3eed57d56835
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-01-04 16:56:26 +0800

    suppress stdout and stderr when game runs, log to Game.log

--- start_warframe.sh
+++ start_warframe.sh
@@ -4,8 +4,8 @@ then
 	WINEEXE=wine
 fi
 
-echo stdout and stderr are scrapped since it\'s known to slow down warframe exe
+echo stdout and stderr are scrapped since it\'s known to slow down warframe exe, logs can be found at \<wine_prefix\>/drive_c/users/\<name\>/Local\ Settings/Application\ Data/Warframe/Game.log
 (
 	cd "$OUTPUT_DIR"
-	$WINEEXE Warframe.x64.exe -fullscreen:1 -graphicsDriver:dx11 -cluster:public -language:en -deferred:0 2> /dev/null > /dev/null
+	$WINEEXE Warframe.x64.exe -silent -log:/Game.log -graphicsDriver:dx11 -cluster:public -language:en -deferred:0 2> /dev/null > /dev/null
 )
